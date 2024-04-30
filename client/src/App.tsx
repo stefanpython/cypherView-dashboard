6b1c2014d967c8dde68b266741488035168db834
stefanpython <stefanhtml@yahoo.com>
2024-04-30 15:46:33 +0300

    Implement login logic, change tab icon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </HashRouter>
     </div>
--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,14 +1,63 @@
+import { FormEvent, useState } from "react";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordErrorm, setPasswordError] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+  const [cookies, setCookies] = useCookies(["token"]);
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      // Perform login logic
+      const res = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+
+      // Check if response is ok
+      if (!res.ok) {
+        const errorData = await res.json();
+        setError(errorData.message);
+        return;
+      }
+
+      // Handle successful login
+      const data = await res.json();
+
+      // Set token in cookies
+      const { token } = data;
+      setCookies("token", token, { path: "/" });
+
+      navigate("/dashboard");
+    } catch (error) {
+      console.log("Login failed", error);
+    }
+  };
+
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32 shadow-lg">
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-400 p-3 md:h-36">
           <div className="w-32 text-white md:w-36 flex items-center">
-            <img className="w-8 mr-1" src="./logo.png" alt="logo" />
+            <img className="w-8 mr-1" src="./layout.png" alt="logo" />
             <p className="text-3xl">CypherView</p>
           </div>
         </div>
-        <form className="space-y-3">
+        <form onSubmit={handleSubmit} className="space-y-3">
           <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
             <h1 className={` mb-3 text-2xl`}>Please log in to continue.</h1>
             <div className="w-full">
@@ -27,6 +76,8 @@ export default function Login() {
                     name="email"
                     placeholder="Enter your email address"
                     required
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
                   />
                 </div>
                 <div className="mt-4">
@@ -44,7 +95,9 @@ export default function Login() {
                       name="password"
                       placeholder="Enter password"
                       required
-                      minLength={6}
+                      minLength={3}
+                      onChange={(e) => setPassword(e.target.value)}
+                      value={password}
                     />
                   </div>
                 </div>
@@ -55,12 +108,11 @@ export default function Login() {
                 aria-live="polite"
                 aria-atomic="true"
               >
-                {/* {errorMessage && (
-            <>
-              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-              <p className="text-sm text-red-500">{errorMessage}</p>
-            </>
-          )} */}
+                {error && (
+                  <div className="bg-red-400 text-white w-fit text-sm py-1 px-3 rounded-sm mt-4 rounded-r-md">
+                    {error}
+                  </div>
+                )}
               </div>
             </div>
           </div>
