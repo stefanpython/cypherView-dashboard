e401002ebdf14092b5ac17ffb41f63ab6f75a455
stefanpython <stefanhtml@yahoo.com>
2024-05-01 17:47:25 +0300

    Create and style invoice searchbar

--- client/src/components/dashboard/Inovices.tsx
+++ client/src/components/dashboard/Inovices.tsx
@@ -1,5 +1,23 @@
-import React from "react";
+import Search from "../invoices/Search";
+import CreateInvoice from "../invoices/CreateInvoice";
+import Table from "../invoices/Table";
 
 export default function Inovices() {
-  return <div>Inovices</div>;
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search />
+        <CreateInvoice />
+      </div>
+
+      <Table />
+
+      <div className="mt-5 flex w-full justify-center">
+        {/* PAGINATION HERE */}
+      </div>
+    </div>
+  );
 }
---
+++ client/src/components/invoices/CreateInvoice.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CreateInvoice() {
+  return <div>CreateInvoice</div>;
+}
