3da46ebdc0f8e32b0055e4eaabac93657a474b00
stefanpython <stefanhtml@yahoo.com>
2024-05-08 17:34:09 +0300

    Fetch and display the latest 5 invoices

--- client/src/components/home/LatestInvoices.tsx
+++ client/src/components/home/LatestInvoices.tsx
@@ -1,44 +1,77 @@
+import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+
 export default function LatestInvoices() {
+  const [cookies, setCookies] = useCookies(["token"]);
+  const [invoices, setInvoices] = useState("");
+
+  // Fetch Invoices
+  const fetchInvoices = async () => {
+    try {
+      const res = await fetch("http://localhost:3000/invoices", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${cookies.token}`,
+        },
+      });
+
+      if (!res.ok) {
+        const invoicesData = await res.json();
+        throw new Error(invoicesData.message);
+      }
+
+      const data = await res.json();
+      setInvoices(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchInvoices();
+  }, []);
+
+  console.log(invoices?.invoices);
+
   return (
     <div className="flex w-full flex-col md:col-span-4">
-      <h2 className={`mb-4 text-xl md:text-2xl`}>Latest Invoices</h2>
-      <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
-        {/* NOTE: comment in this code when you get to this point in the course */}
-
+      <h2 className="mb-4 text-xl md:text-2xl">Latest Invoices</h2>
+      <div className="flex-grow flex-col justify-between rounded-xl bg-gray-50 p-4">
         <div className="bg-white px-6">
-          {/* {latestInvoices.map((invoice, i) => {
-        return (
-          <div
-            key={invoice.id}
-            className={
-              'flex flex-row items-center justify-between py-4'
-            
-            }
-          >
-            <div className="flex items-center">
-             PROFILE PICTURE HERE
-              <div className="min-w-0">
-                <p className="truncate text-sm font-semibold md:text-base">
-                  {invoice.name}
-                </p>
-                <p className="hidden text-sm text-gray-500 sm:block">
-                  {invoice.email}
+          {invoices &&
+            invoices?.invoices.slice(-5).map((invoice) => (
+              <div
+                key={invoice._id}
+                className="flex flex-row items-center justify-between py-4"
+              >
+                <div className="flex items-center">
+                  {/* Display customer profile picture here */}
+                  <img
+                    className="w-10 h-10 rounded-full object-cover mr-2"
+                    src={`http://localhost:3000/images/${invoice.customer.image}`}
+                    alt="customer image"
+                  />
+                  <div className="min-w-0">
+                    {/* Display customer name */}
+                    <p className="truncate text-sm font-semibold md:text-base">
+                      {invoice.customer.firstName} {invoice.customer.lastName}
+                    </p>
+                    {/* Display customer email */}
+                    <p className="hidden text-sm text-gray-500 sm:block">
+                      {invoice.customer.email}
+                    </p>
+                  </div>
+                </div>
+                {/* Display invoice amount */}
+                <p className="truncate text-sm font-medium md:text-base">
+                  $ {invoice.amount}
                 </p>
               </div>
-            </div>
-            <p
-              className={` truncate text-sm font-medium md:text-base`}
-            >
-              {invoice.amount}
-            </p>
-          </div>
-        );
-      })}  MAP OVER INVOICES HERE*/}
+            ))}
         </div>
         <div className="flex items-center pb-2 pt-6">
           <img className="w-4" src="./circle.png" alt="circle" />
-
-          <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
+          <h3 className="ml-2 text-sm text-gray-500">Updated just now</h3>
         </div>
       </div>
     </div>
