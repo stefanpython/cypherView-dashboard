6569a772746454bc5339a0fd3f2ae67ca0b67754
stefanpython <stefanhtml@yahoo.com>
2024-05-21 20:10:43 +0300

    Edit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://stefanpython.github.io/cypherView-dashboard/tree/main/client",
+  base: "https://stefanpython.github.io/cypherView-dashboard",
   plugins: [react()],
 });
